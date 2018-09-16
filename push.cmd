951bf64a265396a1e247767b2b97fa9877873287
320wuyanzu <wuyanzu320@outlook.com>
2018-09-16 21:03:55 +0800

    新版的，可能好看点儿

--- push.cmd
+++ push.cmd
@@ -1,5 +1,5 @@
 chcp 65001
 git status
 git add *
-git commit -m "firefox有毒"
-git push -f git@github.com:320wuyanzu/320wuyanzu.github.io.git master:fuk-firefox
+git commit -m "新版的，可能好看点儿"
+git push -f git@github.com:320wuyanzu/320wuyanzu.github.io.git master:master
